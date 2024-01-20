9f2ae32fba5ccf9dde79e6a3b135c7597e17820a
James Ward <james@notjam.es>
2024-01-19 20:54:56 -0500

    pull config from environment variables

--- seeborg-irc-settings.cpp
+++ seeborg-irc-settings.cpp
@@ -12,7 +12,7 @@ BotSettings botsettings;
 // ---------------------------------------------------------------------------
 
 struct ConfigSetting {
-    const char *configline;
+    const char *env;
     const char *description;
 
     string *stringptr;
@@ -21,68 +21,54 @@ struct ConfigSetting {
 };
 
 static const ConfigSetting configsettings[] = {
-  {"server", "Address of IRC server", &botsettings.server, NULL, NULL},
-  {"serverport", "Server port", NULL, NULL, &botsettings.serverport},
+        {"SEEBORG_SERVER_HOST", "Address of IRC server", &botsettings.server, NULL, NULL},
+        {"SEEBORG_SERVER_PORT", "Server port", NULL, NULL, &botsettings.serverport},
 
-  {"nickname", "Bot's nickname", &botsettings.nickname, NULL, NULL},
-  {"username", "Bot's username (will show as ~<username>@some.host.com)", &botsettings.username, NULL, NULL},
-  {"realname", "Bot's realname (will show in whois)", &botsettings.realname, NULL, NULL},
-  {"quitmessage", "Bot's quit message", &botsettings.quitmessage, NULL, NULL},
+        {"SEEBORG_NICK", "Bot's nickname", &botsettings.nickname, NULL, NULL},
+        {"SEEBORG_USER", "Bot's username (will show as ~<username>@some.host.com)", &botsettings.username, NULL, NULL},
+        {"SEEBORG_REAL", "Bot's realname (will show in whois)", &botsettings.realname, NULL, NULL},
+        {"SEEBORG_QUIT", "Bot's quit message", &botsettings.quitmessage, NULL, NULL},
 
-  {NULL, NULL, NULL, NULL, NULL},      // Newline in cfg
+        {"SEEBORG_REPLY_RATE", "Reply rate to all messages (in percent)", NULL, &botsettings.replyrate, NULL},
+        {"SEEBORG_REPLY_NICK", "Reply rate to messages containing bot's nickname (in percent)", NULL, &botsettings.replyrate_mynick, NULL},
+        {"SEEBORG_REPLY_MAGIC", "Reply rate to messages containing magic words (in percent)", NULL, &botsettings.replyrate_magic, NULL},
 
-  {"replyrate", "Reply rate to all messages (in percent)", NULL, &botsettings.replyrate, NULL},
-  {"replynick", "Reply rate to messages containing bot's nickname (in percent)", NULL, &botsettings.replyrate_mynick, NULL},
-  {"replymagic", "Reply rate to messages containing magic words (in percent)", NULL, &botsettings.replyrate_magic, NULL},
+        {"SEEBORG_SPEAKING", "Controls whether the bot speaks at all (boolean)", NULL, NULL, &botsettings.speaking},
+        {"SEEBORG_LEARNING", "Does the bot learn, or just replies (boolean)", NULL, NULL, &botsettings.learning},
+        {"SEEBORG_STEALTH", "Try to emulate a popular IRC client's behaviour (TODO, boolean)", NULL, NULL, &botsettings.stealth},
+        {"SEEBORG_JOIN_INVITES", "Join the channels the bot was invited to (0 - no, 1 - yes, 2 - only by owner)", NULL, NULL, &botsettings.joininvites},
 
-  {NULL, NULL, NULL, NULL, NULL},      // Newline in cfg
+        {"SEEBORG_CHANNELS", "Channels, delimited by spaces", NULL, NULL, &botsettings.joininvites},
 
-  {"speaking", "Controls whether the bot speaks at all (boolean)", NULL, NULL, &botsettings.speaking},
-  {"learning", "Does the bot learn, or just replies (boolean)", NULL, NULL, &botsettings.learning},
-  {"stealth", "Try to emulate a popular IRC client's behaviour (TODO, boolean)", NULL, NULL, &botsettings.stealth},
-  {"joininvites", "Join the channels the bot was invited to (0 - no, 1 - yes, 2 - only by owner)", NULL, NULL, &botsettings.joininvites},
+        {"SEEBORG_MAGIC_WORDS", "Magic words to trigger the bot, delimited by spaces", NULL, NULL, NULL},
+        {"SEEBORG_OWNERS", "Owners, delimited by spaces", NULL, NULL, NULL},
 
-  {NULL, NULL, NULL, NULL, NULL},      // Newline in cfg
-
-  {"autosaveperiod", "Autosave period (in seconds)", NULL, NULL, &botsettings.autosaveperiod},
-
-  {NULL, NULL, NULL, NULL, NULL}
+        {"SEEBORG_DICTIONARY_FILE", "Autosave period (in seconds)", &botsettings.dictionaryFile, NULL, NULL},
+        {"SEEBORG_DICTIONARY_SAVE_INTERVAL", "Autosave period (in seconds)", NULL, NULL, &botsettings.autosaveperiod},
 };
 
 static const int numconfigsettings =
-    sizeof(configsettings) / sizeof(configsettings[0]) - 1;
+        sizeof(configsettings) / sizeof(configsettings[0]) - 1;
 
 void LoadBotSettings() {
-    string str;
-    ifstream ifs("seeborg-irc.cfg");
-    if (ifs.bad()) {
-        return;
-    }
+    for (int i = 0; i < numconfigsettings; i++) {
+        const ConfigSetting *s = &configsettings[i];
 
-    while (getline(ifs, str)) {
-        trimString(str);
-        if (str[0] == ';' || str[0] == '#' || str.empty()) {
-            continue;
-        }
+        string value = getenv(s->env);
 
-        vector<string> cursetting;
+        // Oh, this is some bad code but whatever, works.
 
-        if (splitString(str, cursetting, "=") < 2) {
-            continue;
-        }
+        bool channels = (!strcasecmp(s->env, "SEEBORG_CHANNELS"));
+        bool owners = (!strcasecmp(s->env, "SEEBORG_OWNERS"));
+        bool magicwords = (!strcasecmp(s->env, "SEEBORG_MAGIC_WORDS"));
 
-        trimString(cursetting[0]);
-        trimString(cursetting[1]);
 
-        bool channels = (!strcasecmp(cursetting[0].c_str(), "channels"));
-        bool owners = (!strcasecmp(cursetting[0].c_str(), "owners"));
-        bool magicwords = (!strcasecmp(cursetting[0].c_str(), "magicwords"));
         if (channels || owners || magicwords) {
             if (channels) {
                 botsettings.channels.clear(); // remove default channels
             }
             vector<string> cursplit;
-            if (!splitString(cursetting[1], cursplit, " ")) {
+            if (!splitString(value, cursplit, " ")) {
                 continue;
             }
             for (auto &s : cursplit) {
@@ -100,22 +86,12 @@ void LoadBotSettings() {
             continue;
         }
 
-        for (int i = 0; i < numconfigsettings; i++) {
-            const ConfigSetting *s = &configsettings[i];
-            if (s->configline == NULL) {
-                continue;
-            }
-            if (!strcasecmp(s->configline, cursetting[0].c_str())) {
-                if (s->stringptr != NULL) {
-                    *s->stringptr = cursetting[1];
-                } else if (s->floatptr != NULL) {
-                    *s->floatptr = atof(cursetting[1].c_str());
-                } else if (s->intptr != NULL) {
-                    *s->intptr = atoi(cursetting[1].c_str());
-                }
-                break;
-            }
+        if (s->stringptr != nullptr) {
+            *s->stringptr = value;
+        } else if (s->floatptr != nullptr) {
+            *s->floatptr = atof(value.c_str());
+        } else if (s->intptr != nullptr) {
+            *s->intptr = atoi(value.c_str());
         }
     }
-    ifs.close();
 }
